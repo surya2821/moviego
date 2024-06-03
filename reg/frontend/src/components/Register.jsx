da616c8c265ae263c86a660c05dff3db0d15553e
Sai Surya Alla <2100031646@kluniversity.in>
2024-06-03 10:28:33 +0530

    Update Register.jsx

--- reg/frontend/src/components/Register.jsx
+++ reg/frontend/src/components/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         
-        axios.post( 'http://localhost:3001/register', {name, email, password})
+        axios.post( 'https://moviego-1ugo.onrender.com:3001/register', {name, email, password})
         .then(result => {
             console.log(result);
             if(result.data === "Already registered"){
